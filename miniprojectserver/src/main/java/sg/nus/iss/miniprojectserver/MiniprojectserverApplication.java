cbe97ac3a894d41866b89eee8d393f76ea776305
JJ How <holyyamateh@hotmail.sg>
2023-03-08 07:46:14 +0800

    initial

---
+++ miniprojectserver/src/main/java/sg/nus/iss/miniprojectserver/MiniprojectserverApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.miniprojectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectserverApplication.class, args);
+	}
+
+}
