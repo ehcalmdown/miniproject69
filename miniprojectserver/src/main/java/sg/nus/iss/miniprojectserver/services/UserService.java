8ee90640eb88cc3e1a5ddfe47dde56fc8deda28b
JJ How <holyyamateh@hotmail.sg>
2023-03-23 14:02:33 +0800

    committed

--- miniprojectserver/src/main/java/sg/nus/iss/miniprojectserver/services/UserService.java
+++ miniprojectserver/src/main/java/sg/nus/iss/miniprojectserver/services/UserService.java
@@ -1,5 +1,17 @@
 package sg.nus.iss.miniprojectserver.services;
 
+import java.util.List;
+
+import sg.nus.iss.miniprojectserver.models.DataTransfer;
+import sg.nus.iss.miniprojectserver.models.User;
+
 public interface UserService {
+    List<DataTransfer> findAllUsers();
+
+    void saveUser(DataTransfer dto);
+
+    User findByEmail(String email);
+
+    
     
 }
