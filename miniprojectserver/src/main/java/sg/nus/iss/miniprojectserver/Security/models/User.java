b0b7bd6ffb7ec71cae1c0b75b6f80d42d033130b
JJ How <holyyamateh@hotmail.sg>
2023-04-17 22:52:58 +0800

    I screwed up

--- miniprojectserver/src/main/java/sg/nus/iss/miniprojectserver/Security/models/User.java
+++ miniprojectserver/src/main/java/sg/nus/iss/miniprojectserver/Security/models/User.java
@@ -27,60 +27,55 @@ import org.springframework.security.core.userdetails.UserDetails;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "_user")
 public class User implements UserDetails {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
-    private String firstname;
-    private String lastname;
-    private String email;
-    private String password;
-  
-    @Enumerated(EnumType.STRING)
-    private Roles roles;
-  
-    @OneToMany(mappedBy = "user")
-    private List<Token> tokens;
-  
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-      return List.of(new SimpleGrantedAuthority(roles.name()));
-    }
-  
-    @Override
-    public String getPassword() {
-      return password;
-    }
-  
-    @Override
-    public String getUsername() {
-      return email;
-    }
-  
-    @Override
-    public boolean isAccountNonExpired() {
-      return true;
-    }
-  
-    @Override
-    public boolean isAccountNonLocked() {
-      return true;
-    }
-  
-    @Override
-    public boolean isCredentialsNonExpired() {
-      return true;
-    }
-  
-    @Override
-    public boolean isEnabled() {
-      return true;
-    }
+  @Id
+  @GeneratedValue
+  private Integer id;
+  private String firstname;
+  private String lastname;
+  private String email;
+  private String password;
 
+  @Enumerated(EnumType.STRING)
+  private Roles roles;
 
-     
+  @OneToMany(mappedBy = "user")
+  private List<Token> tokens;
 
-    
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(roles.name()));
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
