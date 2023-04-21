51d79a9081e960772cebffaef0b7fed0088abbc7
JJ How <holyyamateh@hotmail.sg>
2023-04-22 00:07:26 +0800

    progress

--- miniprojectserver/src/main/java/sg/nus/iss/miniprojectserver/RecipeApp/models/AppUser.java
+++ miniprojectserver/src/main/java/sg/nus/iss/miniprojectserver/RecipeApp/models/AppUser.java
@@ -1,5 +1,23 @@
 package sg.nus.iss.miniprojectserver.RecipeApp.models;
 
+import java.util.List;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppUser {
+    
+        private Long id;
+        private String username;
+        private String password;
+        private List<UserHistory> searchHistory;
     
 }
